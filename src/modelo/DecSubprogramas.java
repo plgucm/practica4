53089ed1ff5cff096e69725baf08890eb748b16b
danielnowendsztern <danielnowens@hotmail.com>
2014-05-29 15:25:38 +0200

    Agregado diag. de clases usando el plugin -> Modelgoon

---
+++ src/modelo/DecSubprogramas.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class DecSubprogramas {
+
+	private DecSubprogramas decSubprogramas;
+
+	public DecSubprogramas getDecSubprogramas() {
+		return this.decSubprogramas;
+	}
+
+	public void setDecSubprogramas(DecSubprogramas decSubprogramas) {
+		this.decSubprogramas = decSubprogramas;
+	}
+
+}
