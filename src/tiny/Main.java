4aa083edb91e5cc04a9a8a1797c6cb46bfdc2d4b
danielnowendsztern <danielnowens@hotmail.com>
2014-06-09 18:10:18 +0200

    Algunas correciones.

--- src/tiny/Main.java
+++ src/tiny/Main.java
@@ -4,11 +4,16 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+import modelo.instrucciones.Programa;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
 		AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
 		AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);		
 		asint.parse();
+		System.out.println("Análisis completado. Se supone que el árbol está construído.");
+		Programa p = asint.programaRaiz;		
+		
 	}
 }
